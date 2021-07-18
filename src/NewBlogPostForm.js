5073c6d4154b70a5273b4574266be7120c032613
claurennt <c.benedetti1989@gmail.com>
2021-07-18 18:44:25 +0200

    fixed axios post body

--- src/NewBlogPostForm.js
+++ src/NewBlogPostForm.js
@@ -2,13 +2,19 @@ import React from "react";
 import axios from "axios";
 import { useState } from "react";
 
+import { Link, useHistory } from "react-router-dom";
+
 axios.defaults.headers.common = {
   "Content-Type": "application/json",
 };
 
 export default function NewBlogPostForms() {
+  const history = useHistory();
+  console.log(history);
   const [newPost, setNewPost] = useState();
   const [token, setToken] = useState();
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isCreated, setIsCreated] = useState(false);
 
   const handleChange = (e) => {
     console.log(
@@ -22,15 +28,11 @@ export default function NewBlogPostForms() {
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(
-      `Creating new POST request with new post data: ${JSON.stringify(
-        newPost
-      )}, ${JSON.stringify(token)}`
-    );
+
     axios
       .post(
         "https://expressful-blog-backend.herokuapp.com/blogPosts/create",
-        { newPost },
+        newPost,
         { headers: { Authorization: `Bearer ${token}` } }
       )
       .then((res) => {
@@ -38,9 +40,11 @@ export default function NewBlogPostForms() {
           console.log("Invalid credentials! Couldn't create new blogpost");
           alert("Invalid credentials. No post created");
         } else {
-          alert("Successfully created a new blogpost!");
+          setIsSuccess(true);
+          setIsCreated(true);
+          // <Link to={`/article/${res.data[0].content_id}`}>Click</Link>;
+          console.log("Successfully created a new blogpost!");
         }
-        console.log({ response: res });
       })
       .catch((error) => {
         console.log("Invalid credentials! Couldn't create new blogpost");
@@ -50,110 +54,125 @@ export default function NewBlogPostForms() {
       });
   };
 
-  //{setError? <p>Access Denied</p>: <p>Thanks for submitting the request</p> }
   return (
     <div className=" d-flex flex-row justify-content-center mt-5 ">
-      <form onSubmit={handleSubmit} className="d-flex flex-column  w-50">
-        <div className="input-group justify-content-center mb-3 w-50">
-          <label className="input-group-text " id="basic-addon1" for="tken">
-            Optimist Token
-          </label>
-          <input
-            onChange={handleToken}
-            type="text"
-            name="token"
-            class="form-control"
-            placeholder="Are you an optimist?"
-            aria-label="title"
-            aria-describedby="basic-addon1"
-          />
-        </div>
-        <h5>Create a new post!</h5>
-        <div className="input-group mb-3">
-          <label className="input-group-text " id="basic-addon1" for="title">
-            Title
-          </label>
-          <input
-            onChange={handleChange}
-            type="text"
-            name="title"
-            class="form-control"
-            placeholder="Insert a title"
-            aria-label="title"
-            aria-describedby="basic-addon1"
-          />
-        </div>
+      {isSuccess && (
+        <>
+          <img
+            src="https://i.giphy.com/media/3oz8xYfQd5358zpL0s/giphy.webp"
+            alt="creation"
+          ></img>
+          <Link>Click here to see your post</Link>
+        </>
+      )}
 
-        <div class="input-group mb-3">
-          <label className="input-group-text" id="basic-addon1" for="mediaUrl">
-            Media Url
-          </label>
-          <input
-            onChange={handleChange}
-            name="media_url"
-            type="text"
-            class="form-control"
-            placeholder="Paste media url"
-            aria-label="Media Url"
-            aria-describedby="basic-addon2"
-          />
-        </div>
+      {!isCreated && (
+        <form onSubmit={handleSubmit} className="d-flex flex-column  w-50">
+          <div className="input-group justify-content-center mb-3 w-50">
+            <label className="input-group-text " id="basic-addon1" for="tken">
+              Optimist Token
+            </label>
+            <input
+              onChange={handleToken}
+              type="text"
+              name="token"
+              class="form-control"
+              placeholder="Are you an optimist?"
+              aria-label="title"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+          <h5>Create a new post!</h5>
+          <div className="input-group mb-3">
+            <label className="input-group-text " id="basic-addon1" for="title">
+              Title
+            </label>
+            <input
+              onChange={handleChange}
+              type="text"
+              name="title"
+              class="form-control"
+              placeholder="Insert a title"
+              aria-label="title"
+              aria-describedby="basic-addon1"
+            />
+          </div>
 
-        <div className="input-group mb-3">
-          <label className="input-group-text " id="basic-addon1" for="author">
-            Author's Name
-          </label>
-          <input
-            onChange={handleChange}
-            type="text"
-            name="author"
-            class="form-control"
-            placeholder="Insert the author name"
-            aria-label="title"
-            aria-describedby="basic-addon1"
-          />
-        </div>
+          <div class="input-group mb-3">
+            <label
+              className="input-group-text"
+              id="basic-addon1"
+              for="mediaUrl"
+            >
+              Media Url
+            </label>
+            <input
+              onChange={handleChange}
+              name="media_url"
+              type="text"
+              class="form-control"
+              placeholder="Paste media url"
+              aria-label="Media Url"
+              aria-describedby="basic-addon2"
+            />
+          </div>
 
-        <div class="input-group mb-3">
-          <label className="input-group-text" for="tags">
-            Choose available tags
-          </label>
-          <select onChange={handleChange} id="tags" name="tags">
-            <option value="energy">energy</option>
-            <option value="environment">environment</option>
-            <option value="gender">gender</option>
-          </select>
-        </div>
-        <div className="input-group mb-3">
-          <label className="input-group-text " id="basic-addon1" for="title">
-            Source News
-          </label>
-          <input
-            onChange={handleChange}
-            type="url"
-            name="source_url"
-            class="form-control"
-            placeholder="Insert a source news url"
-            aria-label="title"
-            aria-describedby="basic-addon1"
-          />
-        </div>
+          <div className="input-group mb-3">
+            <label className="input-group-text " id="basic-addon1" for="author">
+              Author's Name
+            </label>
+            <input
+              onChange={handleChange}
+              type="text"
+              name="author"
+              class="form-control"
+              placeholder="Insert the author name"
+              aria-label="title"
+              aria-describedby="basic-addon1"
+            />
+          </div>
 
-        <div class="input-group">
-          <span class="input-group-text">Post Content</span>
-          <textarea
-            onChange={handleChange}
-            name="body"
-            class="form-control"
-            rows="10"
-            aria-label="Post Content"
-          ></textarea>
-        </div>
+          <div class="input-group mb-3">
+            <label className="input-group-text" for="tags">
+              Choose available tags
+            </label>
+            <select onChange={handleChange} id="tags" name="tags">
+              <option value="energy">energy</option>
+              <option value="environment">environment</option>
+              <option value="gender">gender</option>
+            </select>
+          </div>
+          <div className="input-group mb-3">
+            <label className="input-group-text " id="basic-addon1" for="title">
+              Source News
+            </label>
+            <input
+              onChange={handleChange}
+              type="url"
+              name="source_url"
+              class="form-control"
+              placeholder="Insert a source news url"
+              aria-label="title"
+              aria-describedby="basic-addon1"
+            />
+          </div>
 
-        <div>
-          <button class="btn btn-outline-primary mt-4">Submit</button>
-        </div>
-      </form>
+          <div class="input-group">
+            <span class="input-group-text">Post Content</span>
+            <textarea
+              onChange={handleChange}
+              name="body"
+              class="form-control"
+              rows="10"
+              aria-label="Post Content"
+            ></textarea>
+          </div>
+
+          <div>
+            <button class="btn btn-outline-primary mt-4">Submit</button>
+          </div>
+        </form>
+      )}
     </div>
   );
 }
